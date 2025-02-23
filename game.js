657ac744b37bc43c7f3a175a6c0ae224f4486a86
Cadmusyiu <chunchunyiu0624@gmail.com>
2025-02-23 17:48:17 +0800

    Update game.js

--- game.js
+++ game.js
@@ -29,6 +29,9 @@ class Game {
         if (emptyCells.length > 0) {
             const {r, c} = emptyCells[Math.floor(Math.random() * emptyCells.length)];
             this.board[r][c] = Math.random() < 0.9 ? 2 : 4;
+            console.log(`Added tile ${this.board[r][c]} at [${r},${c}]`);
+        } else {
+            console.log('No empty cells to add new tile');
         }
     }
 
@@ -54,77 +57,87 @@ class Game {
     }
 
     move(direction) {
-    // Create a deep copy of the current board to compare changes
-    const previousBoard = this.board.map(row => [...row]);
-    let moved = false;
+        console.log(`Moving ${direction}`);
+        console.log('Initial board:', this.board);
 
-    // Slide and merge logic
-    const slide = (row) => {
-        // Remove zeros
-        row = row.filter(val => val !== 0);
-        
-        // Merge adjacent equal tiles
-        for (let i = 0; i < row.length - 1; i++) {
-            if (row[i] === row[i + 1]) {
-                row[i] *= 2;
-                this.score += row[i];
-                row.splice(i + 1, 1);
-                moved = true;
+        // Create a deep copy of the current board
+        const previousBoard = this.board.map(row => [...row]);
+        let moved = false;
+
+        // Slide and merge logic
+        const slide = (row) => {
+            // Remove zeros
+            row = row.filter(val => val !== 0);
+            
+            // Merge adjacent equal tiles
+            for (let i = 0; i < row.length - 1; i++) {
+                if (row[i] === row[i + 1]) {
+                    row[i] *= 2;
+                    this.score += row[i];
+                    row.splice(i + 1, 1);
+                    moved = true;
+                }
             }
+            
+            // Pad with zeros
+            while (row.length < this.size) {
+                row.push(0);
+            }
+            
+            return row;
+        };
+
+        // Movement logic for different directions
+        switch(direction) {
+            case 'left':
+                this.board = this.board.map(row => slide(row));
+                break;
+            case 'right':
+                this.board = this.board.map(row => slide([...row].reverse()).reverse());
+                break;
+            case 'up':
+                // Transpose and slide
+                const upTransposed = this.board[0].map((_, colIndex) => 
+                    this.board.map(row => row[colIndex])
+                );
+                const slidUpTransposed = upTransposed.map(row => slide(row));
+                this.board = slidUpTransposed[0].map((_, colIndex) => 
+                    slidUpTransposed.map(row => row[colIndex])
+                );
+                break;
+            case 'down':
+                // Transpose and slide
+                const downTransposed = this.board[0].map((_, colIndex) => 
+                    this.board.map(row => row[colIndex])
+                );
+                const slidDownTransposed = downTransposed.map(row => slide([...row].reverse()).reverse());
+                this.board = slidDownTransposed[0].map((_, colIndex) => 
+                    slidDownTransposed.map(row => row[colIndex])
+                );
+                break;
+        }
+
+        console.log('Board after move:', this.board);
+
+        // Check for empty cells
+        const emptyCell = this.hasEmptyCell();
+        console.log('Has empty cell:', emptyCell);
+
+        // Check if the board actually changed
+        const boardChanged = !this.board.every((row, rowIndex) => 
+            row.every((cell, colIndex) => cell === previousBoard[rowIndex][colIndex])
+        );
+        console.log('Board changed:', boardChanged);
+
+        // Always try to add a new tile if there's an empty cell
+        if (emptyCell) {
+            this.addRandomTile();
         }
         
-        // Pad with zeros
-        while (row.length < this.size) {
-            row.push(0);
-        }
-        
-        return row;
-    };
-
-    // Movement logic for different directions
-    switch(direction) {
-        case 'left':
-            this.board = this.board.map(row => slide(row));
-            break;
-        case 'right':
-            this.board = this.board.map(row => slide([...row].reverse()).reverse());
-            break;
-        case 'up':
-            // Transpose and slide
-            const upTransposed = this.board[0].map((_, colIndex) => 
-                this.board.map(row => row[colIndex])
-            );
-            const slidUpTransposed = upTransposed.map(row => slide(row));
-            this.board = slidUpTransposed[0].map((_, colIndex) => 
-                slidUpTransposed.map(row => row[colIndex])
-            );
-            break;
-        case 'down':
-            // Transpose and slide
-            const downTransposed = this.board[0].map((_, colIndex) => 
-                this.board.map(row => row[colIndex])
-            );
-            const slidDownTransposed = downTransposed.map(row => slide([...row].reverse()).reverse());
-            this.board = slidDownTransposed[0].map((_, colIndex) => 
-                slidDownTransposed.map(row => row[colIndex])
-            );
-            break;
+        this.updateBoard();
+        return boardChanged;
     }
 
-    // Check if the board actually changed
-    const boardChanged = !this.board.every((row, rowIndex) => 
-        row.every((cell, colIndex) => cell === previousBoard[rowIndex][colIndex])
-    );
-
-    // Add new tile if board changed and there's an empty cell
-    if (boardChanged && this.hasEmptyCell()) {
-        this.addRandomTile();
-    }
-    
-    this.updateBoard();
-    return boardChanged;
-}
-    // Check if there are any empty cells
     hasEmptyCell() {
         for (let r = 0; r < this.size; r++) {
             for (let c = 0; c < this.size; c++) {
@@ -163,11 +176,14 @@ let touchStartX = 0;
 let touchStartY = 0;
 
 document.getElementById('game-board').addEventListener('touchstart', (e) => {
+    e.preventDefault();
     touchStartX = e.touches[0].clientX;
     touchStartY = e.touches[0].clientY;
 }, { passive: false });
 
 document.getElementById('game-board').addEventListener('touchend', (e) => {
+    e.preventDefault();
+    
     const touchEndX = e.changedTouches[0].clientX;
     const touchEndY = e.changedTouches[0].clientY;
 
